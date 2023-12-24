ff9aa3fe718c3b2277d6a25ad3d4b5edcf917666
Marlik Almighty <cryptocoin62@gmail.com>
2023-12-24 06:35:27 +0300

    up test

--- internal/config/get_env_test.go
+++ internal/config/get_env_test.go
@@ -8,23 +8,15 @@ import (
 
 func TestConfiguration_GetEnv(t *testing.T) {
 
-	if err := os.Setenv("HTTP_HOST", "127.0.0.1"); err != nil {
-		t.Errorf("Error: %v", err)
-	}
-
 	if err := os.Setenv("HTTP_PORT", "8081"); err != nil {
 		t.Errorf("Error: %v", err)
 	}
 
-	if err := os.Setenv("DNS_HOST", "0.0.0.0"); err != nil {
+	if err := os.Setenv("DNS_TCP_PORT", "1053"); err != nil {
 		t.Errorf("Error: %v", err)
 	}
 
-	if err := os.Setenv("DNS_TCP_PORT", "5353"); err != nil {
-		t.Errorf("Error: %v", err)
-	}
-
-	if err := os.Setenv("DNS_UDP_PORT", "5353"); err != nil {
+	if err := os.Setenv("DNS_UDP_PORT", "1053"); err != nil {
 		t.Errorf("Error: %v", err)
 	}
 
@@ -47,9 +39,7 @@ func TestConfiguration_GetEnv(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			cnf := &Configuration{
-				HTTPHost:    tt.fields.HTTPHost,
 				HTTPPort:    tt.fields.HTTPPort,
-				DnsHost:     tt.fields.DnsHost,
 				DnsTcpPort:  tt.fields.DnsTcpPort,
 				DnsUdpPort:  tt.fields.DnsUdpPort,
 				NameServers: tt.fields.NameServers,
